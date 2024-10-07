a479219d0f630a61f50ebc64d7c608778a85253d
Andres Olaya <80360942+andres123o@users.noreply.github.com>
2024-10-07 17:48:04 -0500

    Actualizacion de diseños y configuracion de seccion de descripcion de bares y restaurantes

--- src/components/actividades/descripcion-actividades.js
+++ src/components/actividades/descripcion-actividades.js
@@ -1,44 +1,45 @@
-const DescripcionActividades = () => {
-    const [backgroundImage, setBackgroundImg] = useState(rutas.img);
+const DescripcionActividades = (activida ) => {
+    // const [backgroundImage, setBackgroundImg] = useState(rutas.img);
 
 
-    useEffect(() => {
-      setBackgroundImg(rutas.img)
-    }, [rutas.img])
+    // useEffect(() => {
+    //   setBackgroundImg(rutas.img)
+    // }, [rutas.img])
   
-    const handleImageSelect = (imgSrc) => {
-      setBackgroundImg(imgSrc);
-    };
+    // const handleImageClick = (imgSrc, index) => {
+    //     setSelectedImgIndex(index);
+    //     onImageSelect(imgSrc);
+    //   };
 
-    return (
-        <>
-            {/* Ver las imagenes del carrusel */}
-            <div 
-                className='container-img-principal' 
-                style={{
-                backgroundImage:`url(${backgroundImage})`
-                }}
-            >
-            </div>
+    // return (
+    //     <>
+    //         {/* Ver las imagenes del carrusel */}
+    //         <div 
+    //             className='container-img-principal' 
+    //             style={{
+    //             backgroundImage:`url(${backgroundImage})`
+    //             }}
+    //         >
+    //         </div>
                 
-            {/* Container principal de la informacion */}
-            <div className="container-info">
-                <div className='container-carrusel-imgs'>
-                    <div className='carrusel-imgs'>
-                        {rutas.imgs.map((imgSrc, index) => (
-                        <img 
-                            key={index}
-                            src={imgSrc}
-                            alt="Imagen de la ruta"
-                            onClick={() => handleImageClick(imgSrc, index)}
-                            className={index === selectedImgIndex ? 'selected' : ''}
-                        />
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+    //         {/* Container principal de la informacion */}
+    //         <div className="container-info">
+    //             <div className='container-carrusel-imgs'>
+    //                 <div className='carrusel-imgs'>
+    //                     {rutas.imgs.map((imgSrc, index) => (
+    //                     <img 
+    //                         key={index}
+    //                         src={imgSrc}
+    //                         alt="Imagen de la ruta"
+    //                         onClick={() => handleImageClick(imgSrc, index)}
+    //                         className={index === selectedImgIndex ? 'selected' : ''}
+    //                     />
+    //                     ))}
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </>
+    // )
 }
 
 export default DescripcionActividades;
