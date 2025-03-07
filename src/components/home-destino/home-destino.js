4045eb827b95ac2d15c5ecfcaa5079d1fafb9e9d
andres123o <andresolayacris@gmail.com>
2025-03-07 10:54:18 -0500

    Cambiar banner de ad 2

--- src/components/home-destino/home-destino.js
+++ src/components/home-destino/home-destino.js
@@ -162,7 +162,7 @@ const Homedestino = () => {
                 destino_id={destino_id}
             />
             <div className='banner-ads'>
-                <img src='https://res.cloudinary.com/destinoplus/image/upload/v1741362492/dpfn8tgfx4yyjshcso9u.jpg'/>
+                <img src='https://res.cloudinary.com/destinoplus/image/upload/v1741362727/pptjxoehzinr0ciqxjyy.jpg'/>
             </div>
             <AccommodationsSection 
                 alojamientos={alojamientos} 
